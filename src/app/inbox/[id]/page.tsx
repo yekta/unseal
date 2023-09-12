e0fb11e696f47c009482f97cdf9f22ffa59f7950
yekta <ngyekta@gmail.com>
2023-09-13 02:13:08 +0300

    Navbar animation tweak

--- src/app/inbox/[id]/page.tsx
+++ src/app/inbox/[id]/page.tsx
@@ -13,7 +13,7 @@ export default function Page({ params }: { params: { id: string } }) {
             href="/"
             className="flex-1 min-w-[4rem] min-h-full cursor-default"
           />
-          <div className="w-full max-w-6xl flex md:px-4 flex-col relative pt-6 pb-24">
+          <div className="w-full max-w-6xl flex md:px-4 flex-col relative pt-12 pb-24">
             <div className="w-full bg-c-bg-secondary flex flex-col py-6 px-10 rounded-xl">
               <h1 className="w-full font-bold text-3xl">{email?.title}</h1>
               <p className="mt-2">{email?.body}</p>
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -43,63 +43,94 @@ export default function TitleBar() {
     setHoveredPath(pathname);
   }, [pathname]);
 
+  const windowButtonClasses = "w-[13px] h-[13px] rounded-full";
+  const windowButtonContainerClasses =
+    "flex items-center justify-start py-3 px-3 gap-[8px]";
+
   return (
     <nav
       data-tauri-drag-region
       className="w-full flex items-center justify-between bg-c-bg z-[9999] border-b-2 border-c-bg-secondary overflow-hidden relative"
     >
-      <div className="w-40 flex items-center justify-start py-3 px-3 gap-[8px]">
+      <div data-tauri-drag-region className={windowButtonContainerClasses}>
         <button
-          className="w-[13px] h-[13px] rounded-full bg-c-macos-red"
+          className={`${windowButtonClasses} bg-c-macos-red`}
           onClick={() => appWindow?.close()}
         ></button>
         <button
-          className="w-[13px] h-[13px] rounded-full bg-c-macos-orange"
+          className={`${windowButtonClasses} bg-c-macos-orange`}
           onClick={() => appWindow?.minimize()}
         ></button>
         <button
-          className="w-[13px] h-[13px] rounded-full bg-c-macos-green"
+          className={`${windowButtonClasses} bg-c-macos-green`}
           onClick={() => appWindow?.maximize()}
         ></button>
       </div>
-      <div className="flex-1 flex items-center justify-center">
-        {navbarItems.map((item: TNavbarItem) => {
-          const isActive = pathname === item.pathname;
-          return (
-            <Link
-              onMouseOver={() => setHoveredPath(item.pathname)}
-              onMouseLeave={() => setHoveredPath(pathname)}
-              href={item.pathname}
-              className="p-1.5 group cursor-default"
-            >
-              <div className="relative">
-                {item.pathname === hoveredPath && (
-                  <motion.div
-                    className="absolute bottom-0 left-0 h-full bg-c-bg-highlight rounded-lg -z-10"
-                    layoutId="navbar"
-                    aria-hidden="true"
-                    style={{
-                      width: "100%",
-                    }}
-                    transition={{
-                      duration: 0.15,
-                      ease: "circOut",
-                    }}
-                  />
-                )}
-                <p
-                  className={`px-4 py-2 font-medium transition duration-150 ${
-                    isActive ? "text-c-on-bg" : "text-c-on-bg/60"
-                  }`}
-                >
-                  {item.label}
-                </p>
-              </div>
-            </Link>
-          );
-        })}
+      <div
+        data-tauri-drag-region
+        className="flex-1 flex items-center justify-center"
+      >
+        <div className="flex items-center justify-center overflow-hidden relative z-0 rounded-xl">
+          <div className="absolute left-0 top-0 w-full h-full p-1.5 pointer-events-none">
+            {hoveredPath === undefined ||
+              (!navbarItems.some((i) => i.pathname === hoveredPath) && (
+                <motion.div
+                  style={{
+                    width: "100%",
+                    opacity: 0,
+                  }}
+                  className="h-full bg-c-bg-highlight rounded-lg -z-10"
+                  layoutId="navbar"
+                  aria-hidden="true"
+                  transition={{
+                    duration: 0.3,
+                    ease: "circOut",
+                  }}
+                />
+              ))}
+          </div>
+          {navbarItems.map((item: TNavbarItem) => {
+            const isActive = pathname === item.pathname;
+            return (
+              <Link
+                onMouseOver={() => setHoveredPath(item.pathname)}
+                onMouseLeave={() => setHoveredPath(pathname)}
+                href={item.pathname}
+                className="p-1.5 group cursor-default"
+              >
+                <div className="relative">
+                  {item.pathname === hoveredPath && (
+                    <motion.div
+                      style={{
+                        width: "100%",
+                      }}
+                      className="absolute bottom-0 left-0 h-full bg-c-bg-highlight rounded-lg -z-10 pointer-events-none"
+                      layoutId="navbar"
+                      aria-hidden="true"
+                      transition={{
+                        duration: 0.15,
+                        ease: "circOut",
+                      }}
+                    />
+                  )}
+                  <p
+                    className={`px-4 py-2 font-medium transition duration-150 ${
+                      isActive ? "text-c-on-bg" : "text-c-on-bg/60"
+                    }`}
+                  >
+                    {item.label}
+                  </p>
+                </div>
+              </Link>
+            );
+          })}
+        </div>
       </div>
-      <div className="w-40"></div>
+      <div data-tauri-drag-region className={windowButtonContainerClasses}>
+        <div className={windowButtonClasses}></div>
+        <div className={windowButtonClasses}></div>
+        <div className={windowButtonClasses}></div>
+      </div>
     </nav>
   );
 }
