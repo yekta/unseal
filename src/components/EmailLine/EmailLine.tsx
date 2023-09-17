631992cbd315d0ce700261e02ec5c36ee9a92102
yekta <ngyekta@gmail.com>
2023-09-18 02:49:25 +0300

    Color tweaks

--- src/components/EmailLine/EmailLine.tsx
+++ src/components/EmailLine/EmailLine.tsx
@@ -53,14 +53,14 @@ export function EmailLine({
             </p>
             <p
               className={`md:hidden w-16 text-right text-base ${
-                isRead ? "text-c-on-bg/60" : "text-c-on-bg"
+                isRead ? "text-c-on-bg/50" : "text-c-on-bg"
               }`}
             >
               {getRelativeDate(date)}
             </p>
           </div>
           {/* Desktop */}
-          <p className="hidden md:block whitespace-nowrap flex-1 overflow-hidden overflow-ellipsis text-c-on-bg/60 text-base">
+          <p className="hidden md:block whitespace-nowrap flex-1 overflow-hidden overflow-ellipsis text-c-on-bg/50 text-base">
             <span
               className={`${
                 isRead
@@ -72,14 +72,14 @@ export function EmailLine({
             </span>
             <span className={`text-c-on-bg/30 px-0.3ch`}> | </span>
             <span
-              className={`${isRead ? "text-c-on-bg/60" : "text-c-on-bg/75"}`}
+              className={`${isRead ? "text-c-on-bg/50" : "text-c-on-bg/75"}`}
             >
               {body}
             </span>
           </p>
           <p
             className={`hidden md:block w-18 text-right text-base ${
-              isRead ? "text-c-on-bg/60" : "text-c-on-bg"
+              isRead ? "text-c-on-bg/50" : "text-c-on-bg"
             }`}
           >
             {getRelativeDate(date)}
@@ -97,7 +97,7 @@ export function EmailLine({
           </p>
           <p
             className={`md:hidden w-full whitespace-nowrap overflow-hidden overflow-ellipsis mt-1 text-base
-            ${isRead ? "text-c-on-bg/60" : "text-c-on-bg/75"}`}
+            ${isRead ? "text-c-on-bg/50" : "text-c-on-bg/75"}`}
           >
             {body}
           </p>
@@ -115,7 +115,7 @@ const NotificationDot = ({
   className?: string;
 }) => (
   <div
-    className={`w-1.75 h-1.75 md:absolute md:-left-4 md:top-1/2 transform md:-translate-y-1/2 
+    className={`w-2 h-2 md:absolute md:-left-4 md:top-1/2 transform md:-translate-y-1/2 
     rounded-full flex-shrink-0 ${
       isRead ? "bg-c-notification/0 hidden" : "bg-c-notification"
     } ${className ?? ""}`}
