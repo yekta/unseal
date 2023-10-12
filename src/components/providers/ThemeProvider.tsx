26e7b6cfc9adc6f10a90604c8bd52920621a22bc
yekta <ngyekta@gmail.com>
2023-10-12 22:23:33 +0300

    Remove useEffect

--- src/components/providers/ThemeProvider.tsx
+++ src/components/providers/ThemeProvider.tsx
@@ -1,4 +1,4 @@
-import { atom, useAtom, useAtomValue } from "jotai";
+import { atom, useAtom, useAtomValue, useSetAtom } from "jotai";
 import { atomWithStorage } from "jotai/utils";
 import { useEffect } from "react";
 
@@ -10,33 +10,31 @@ export const themeAtom = atomWithStorage<TTheme>("theme", defaultTheme);
 export const systemThemeAtom = atom<TSystemTheme>("light");
 
 export function ThemeProvider({ children }: { children: React.ReactNode }) {
-  const [systemTheme, setSystemTheme] = useAtom(systemThemeAtom);
+  const setSystemTheme = useSetAtom(systemThemeAtom);
   const theme = useAtomValue(themeAtom);
 
-  useEffect(() => {
-    const updateTheme = () => {
-      if (
-        window.matchMedia &&
-        window.matchMedia("(prefers-color-scheme: dark)").matches
-      ) {
-        if (systemTheme !== "dark") {
-          setSystemTheme("dark");
-        }
-      } else {
-        if (systemTheme !== "light") {
-          setSystemTheme("light");
-        }
-      }
-    };
+  const setSystemThemeAndDocumentAttributes = (systemTheme: TSystemTheme) => {
+    setSystemTheme(systemTheme);
+    setDocumentAttributes(theme, systemTheme);
+  };
 
-    updateTheme();
+  useEffect(() => {
+    if (
+      window.matchMedia &&
+      window.matchMedia("(prefers-color-scheme: dark)").matches
+    ) {
+      setSystemThemeAndDocumentAttributes("dark");
+    } else {
+      setSystemThemeAndDocumentAttributes("light");
+    }
 
     const darkMatcher = window.matchMedia("(prefers-color-scheme: dark)");
+
     const handleChange = (e: MediaQueryListEvent) => {
       if (e.matches) {
-        setSystemTheme("dark");
+        setSystemThemeAndDocumentAttributes("dark");
       } else {
-        setSystemTheme("light");
+        setSystemThemeAndDocumentAttributes("light");
       }
     };
 
@@ -46,27 +44,33 @@ export function ThemeProvider({ children }: { children: React.ReactNode }) {
     };
   }, []);
 
-  useEffect(() => {
-    if (theme === "light" || (theme === "system" && systemTheme === "light")) {
-      if (document.documentElement.getAttribute("data-theme") !== "light") {
-        document.documentElement.setAttribute("data-theme", "light");
-      }
-    } else {
-      if (document.documentElement.getAttribute("data-theme") !== "dark") {
-        document.documentElement.setAttribute("data-theme", "dark");
-      }
-    }
-  }, [theme, systemTheme]);
-
   return children;
 }
 
 export function useTheme() {
   const [theme, setTheme] = useAtom(themeAtom);
   const systemTheme = useAtomValue(systemThemeAtom);
+
+  const setThemeAndDocumentAttributes = (theme: TTheme) => {
+    setTheme(theme);
+    setDocumentAttributes(theme, systemTheme);
+  };
+
   return {
     theme,
     systemTheme,
-    setTheme,
+    setTheme: setThemeAndDocumentAttributes,
   };
+}
+
+function setDocumentAttributes(theme: TTheme, systemTheme: TSystemTheme) {
+  if (theme === "light" || (theme === "system" && systemTheme === "light")) {
+    if (document.documentElement.getAttribute("data-theme") !== "light") {
+      document.documentElement.setAttribute("data-theme", "light");
+    }
+  } else {
+    if (document.documentElement.getAttribute("data-theme") !== "dark") {
+      document.documentElement.setAttribute("data-theme", "dark");
+    }
+  }
 }
